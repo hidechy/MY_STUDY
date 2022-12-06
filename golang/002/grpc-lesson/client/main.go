c849aa8ee2873b1c448595b61a0429c9de0e9dbd
toyohide <hide.toyoda@gmail.com>
2022-12-07 07:10:43 +0900

    commit 20221207

---
+++ golang/002/grpc-lesson/client/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"grpc-lesson/pb"
+	"io"
+	"log"
+)
+
+func main() {
+	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("Failed to connect: %v", err)
+	}
+
+	defer conn.Close()
+
+	client := pb.NewFileServiceClient(conn)
+
+	callListFiles(client)
+
+	//
+	callDownload(client)
+	//
+
+}
+
+func callListFiles(client pb.FileServiceClient) {
+	res, err := client.ListFiles(context.Background(), &pb.ListFilesRequest{})
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Println(res.GetFilenames())
+}
+
+//
+func callDownload(client pb.FileServiceClient) {
+	req := &pb.DownloadRequest{Filename: "name.txt"}
+	stream, err := client.Download(context.Background(), req)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	for {
+		res, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		log.Printf("Response from Download(bytes): %v", res.GetData())
+		log.Printf("Response from Download(string): %v", string(res.GetData()))
+	}
+}
+
+//
